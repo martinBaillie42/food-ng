1d382ff1a3b7c240a0623e9d539babdc41ee4c07
martin.baillie <martin.baillie@mnetics.co.uk>
2018-03-29 22:34:17 +0100

    Create Recipe module, routes and component.

---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'food-recipe',
+    templateUrl: './recipe.component.html',
+    styleUrls: ['./recipe.component.scss']
+})
+export class RecipeComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
